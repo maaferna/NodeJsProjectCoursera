f395a824fb0e13b6104d8ab9672eb76e7fc51b89
mafparra <mafernandezparra@gmail.com>
2022-10-07 19:08:38 -0300

    It was added examples of callback and handling error

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,17 +1,22 @@
+const rectangle = require('./rectangle');
 var rect = require('./rectangle');
 
 
 function solverRect(l,b) {
     console.log("Solving for rectangule with = " +  l +"and b = " +b);
-    if ( l <=0 || b <=0) {
-        console.log("Error: Rectangule dimensions should be greater than zero")
-    }
-    else {
-        console.log("The area of the rectangle is " + rect.area(l,b))
-        console.log("The perimer of the rectangle is " + rect.perimeter(l,b))
-    }
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = " + l
+            + ", and b = " + b + " is: " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = " + l
+            + ", and b = " + b + " is: " + rectangle.perimeter());
+        }
+    });
 }
-
+console.log("This statment is after the call to rectangle function")
 solverRect(3, 4);
 solverRect(0, 5);
 solverRect(-3, 6);

fc48e4ac304f386785a3f9e5a80c8a6000e6fad6
mafparra <mafernandezparra@gmail.com>
2022-10-11 22:49:28 -0300

    It was added router with express, Use the Express Router in Express framework to support REST API

--- node-express/index.js
+++ node-express/index.js
@@ -4,12 +4,65 @@ const http = require('http');
 //declare use of Morgan
 const morgan = require('morgan');
 
+
+// configure router with express 
+const bodyParser = require('body-parser');
+
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev')); //use morgan in the developement stage
 app.use(express.static(__dirname + '/public'));
+
+// declare routes
+app.use(bodyParser.json());
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' +  req.body.name 
+            + 'with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on in dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId+
+    'to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on in dishes/: ' +
+    req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Will update the dish: ' + req.params.dishId +
+        '\n');
+    res.end('Will update the dish: ' + req.body.name 
+    + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 app.use((req, res, next) => {
     res.statusCode = 200;
@@ -21,6 +74,8 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
+
+
 
 
 

acc7e3da4ef7b0d31fc27d04309f4b15defde851
mafparra <mafernandezparra@gmail.com>
2022-10-13 21:37:36 -0300

    Generating an Express application using the express-generator
    Modify the Express application to support the REST API by adding routes

---
+++ conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+leaderRouter.use(express.json());
+
+leaderRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res, next) => {
+    res.end('Will send all the leaders to you!');
+})
+
+.post((req, res, next) => {
+    res.end('Will add the leader: ' +  req.body.name 
+            + 'with details: ' + req.body.description);
+})
+
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on in leaders');
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting all the leaders');
+});
+
+
+leaderRouter.route('/:leaderId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res, next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId+
+    'to you!');
+})
+
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on in leaders/: ' +
+    req.params.leaderId)
+})
+.put((req, res, next) => {
+    res.write('Will update the leader: ' + req.params.leaderId +
+        '\n');
+    res.end('Will update the leader: ' + req.body.name 
+    + ' with details: ' + req.body.description);
+})
+
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+
+module.exports = leaderRouter;
